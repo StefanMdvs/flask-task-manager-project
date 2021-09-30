9a000a8e71f534b19ee080db1d649723ec6277b9
StefanMdvs <stefan.medves@gmail.com>
2021-09-30 09:43:24 +0000

    Connect Flask to MongoDb

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, 
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 """
 Since we are not going to push the env.py file to GitHub, once our app is deployed to
 Heroku, it won't be able to find the env.py file, so it will throw an error.
@@ -13,10 +17,18 @@ if os.path.exists("env.py"):
 # create an instance of Flask
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Salut lume!"
+@app.route("/get_tasks")
+def get_tasks():
+    tasks = mongo.db.tasks.find()
+    return render_template("tasks.html", tasks=tasks)
 
 
 if __name__ == "__main__":
